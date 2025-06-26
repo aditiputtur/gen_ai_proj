6fa012291d1206649ef0502f16a4c194f03f6524
Aditi Puttur <aditi.puttur@gmail.com>
2025-06-26 14:42:32 -0700

    ui

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from models import RequestBody, ResponseBody
 from logic import get_patient_guide
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,3 +9,11 @@ app = FastAPI()
 def generate(data: RequestBody):
     guide = get_patient_guide(data.user_input)
     return {"response":guide}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins for now (good for testing)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
